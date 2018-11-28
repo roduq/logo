433f6945234808abc9ef9f3ea386ef27dd22f099
roduq <45419153+roduq@users.noreply.github.com>
2018-11-28 10:35:24 +0100

    Update index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://raw.githubusercontent.com/roduq/logo/master/data.json'
+})
