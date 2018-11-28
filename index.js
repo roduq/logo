c01c4ca8eafea06587ce24f8b09b1d96035d329e
roduq <45419153+roduq@users.noreply.github.com>
2018-11-28 10:35:14 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('bm'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: 'https://raw.githubusercontent.com/roduq/logo/master/data.json'
-})
