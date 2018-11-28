84d57d0c647f4a55366dc1557fe6ed6a0248fda7
roduq <45419153+roduq@users.noreply.github.com>
2018-11-28 10:24:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://raw.githubusercontent.com/roduq/logo/master/data.json'
+})
